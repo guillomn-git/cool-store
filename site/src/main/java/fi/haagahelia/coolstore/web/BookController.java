e6b1fffce30724097d1f89f5b0b9807aae89b1a3
Guillaume Chaumont <112415471+guillomn-git@users.noreply.github.com>
2022-09-08 10:35:03 +0300

    Add files via upload

--- site/src/main/java/fi/haagahelia/coolstore/web/BookController.java
+++ site/src/main/java/fi/haagahelia/coolstore/web/BookController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import fi.haagahelia.coolstore.domain.Book;
 import fi.haagahelia.coolstore.domain.BookRepository;
 
 @Controller
@@ -18,5 +20,17 @@ public class BookController {
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
+	}
+
+	@RequestMapping(value = "/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
 	}
 }
