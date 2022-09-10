7e6014aa571948ed833fc85dcc2b8d373a0d40b3
Guillaume Chaumont <112415471+guillomn-git@users.noreply.github.com>
2022-09-10 14:56:59 +0300

    Add files via upload

--- site/src/main/java/fi/haagahelia/coolstore/web/BookController.java
+++ site/src/main/java/fi/haagahelia/coolstore/web/BookController.java
@@ -4,8 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,6 +28,12 @@ public class BookController {
 	@Autowired
 	private CategoryRepository crepository;
 
+	// Login page
+	@RequestMapping(value = "/login")
+	public String showLogin() {
+		return "login";
+	}
+
 	// REST Methods
 	// Get all books in JSON
 	@RequestMapping(value = "/books", method = RequestMethod.GET)
@@ -40,13 +48,14 @@ public class BookController {
 	}
 
 	// HTML Methods
-	@RequestMapping(value = { "/", "/booklist" })
+	@GetMapping(value = { "/", "/booklist" })
 	public String bookList(Model model) {
 		model.addAttribute("books", brepository.findAll());
 		return "booklist";
 	}
 
 	@RequestMapping(value = "/add")
+	@PreAuthorize("hasRole('ADMIN')")
 	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
 		model.addAttribute("categories", crepository.findAll());
@@ -60,12 +69,14 @@ public class BookController {
 	}
 
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	@PreAuthorize("hasRole('ADMIN')")
 	public String deleteBook(@PathVariable("id") Long id, Model model) {
 		brepository.deleteById(id);
 		return "redirect:../booklist";
 	}
 
 	@RequestMapping(value = "/edit/{id}")
+	@PreAuthorize("hasRole('ADMIN')")
 	public String editBook(@PathVariable("id") Long id, Model model) {
 		model.addAttribute("book", brepository.findById(id));
 		model.addAttribute("categories", crepository.findAll());
